21cddcf35df9383b44af449e6512ef0c846035c9
Ricardo André <91620783+rickzerahh@users.noreply.github.com>
2025-11-27 21:38:21 -0300

    hide navbar l.ogin screen

--- FilmeFlix/app/_layout.tsx
+++ FilmeFlix/app/_layout.tsx
@@ -1,13 +1,16 @@
-import { Stack } from "expo-router";
+import { Stack, usePathname } from "expo-router";
 import { StatusBar, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Navbar from "../destaques/Navbar";
 
 export default function Layout() {
+  const pathname = usePathname();
+  const hideNavbar = pathname === "/login";
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#0d0d0d" }}>
       <StatusBar barStyle="light-content" backgroundColor="#0d0d0d" />
-      <Navbar />
+      {!hideNavbar && <Navbar />}
       <View style={{ flex: 1 }}>
         <Stack
           screenOptions={{
